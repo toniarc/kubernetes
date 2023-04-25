3729b912b1d54bf646e00c508a42b51b9a0d4309
Antonio Junior <antonio.junior@prodepa.pa.gov.br>
2023-04-25 08:07:46 -0300

    first commit

---
+++ server.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", Hello)
+	http.ListenAndServe(":8080", nil)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+	fmt.Fprintf(w, "<h1>Hello, I'm %s and I'm %s.!!!</h1>", name, age)
+}
